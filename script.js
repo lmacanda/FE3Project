974ba174adb4c90182ac004597c8b464b743da46
fadafful <fadafful@gmail.com>
2023-08-17 15:06:19 +0000

    tab function complete

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ navToggle.addEventListener("click", () => {
   closeIcon.classList.toggle("active");
   menuIcon.classList.toggle("active");
 });
+
+
+/* Discover More NFTs Tabs */
+const nftTabButtons = document.querySelectorAll(".disc-nft-cat-tab-left-tablinks");
+nftTabButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const tabcontent = document.querySelectorAll(".disc-nft-cat-tabcontent");
+    tabcontent.forEach((tab) => {
+      tab.style.display = "none";
+    });
+
+    nftTabButtons.forEach((button) => {
+      button.className = button.className.replace(" active", "");
+    });
+
+    document.getElementById(e.target.textContent.split(' ').join('').toLowerCase()).style.display = "block";
+    e.target.className += " active";
+  });
+});
+
+document.getElementById("defaultTab").click();
+/* Discover More NFTs Tabs */
