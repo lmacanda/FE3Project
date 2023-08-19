e0df66a2a8b944cb336ecea4061e3d36cc8de13c
fadafful <fadafful@gmail.com>
2023-08-19 07:04:20 +0000

    Mobile responsiveness in progress

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// https://www.figma.com/file/zWwZjOIjiTSgBn1cIMhuEp/Jagaad-Module-3-Project?node-id=0%3A35&mode=dev
-
-//Navbar
-
-const navToggle = document.querySelector(".top_menu_mobile_nav_toggle");
-const topMenuNav = document.querySelector(".top_menu_nav");
-const closeIcon = document.querySelector(
-  ".top_menu_mobile_nav_toggle_icon_close_menu"
-);
-const menuIcon = document.querySelector(
-  ".top_menu_mobile_nav_toggle_icon_menu"
-);
-
-navToggle.addEventListener("click", () => {
-  topMenuNav.classList.toggle("opened");
-  closeIcon.classList.toggle("active");
-  menuIcon.classList.toggle("active");
-});
-
-
 /* Discover More NFTs Tabs */
 const nftTabButtons = document.querySelectorAll(".disc-nft-ctr-cat-tab-left-tablinks");
 nftTabButtons.forEach((button) => {
